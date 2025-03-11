f4899b297eac868f379be545e2cdb96a880853dc
aslon <aslon@adsfasdjkl/cp.com>
2025-03-11 06:46:47 -0400

    some change

--- server.py
+++ server.py
@@ -214,7 +214,7 @@ class Client:
             wav_file.setframerate(sample_rate)  # Sample rate (e.g., 16kHz)
             wav_file.writeframes(self.buffer)  # Write raw PCM data
 
-       BASE_URL = f"http://{os.getenv('TRANSCRIBER_HOST')}:{os.getenv('TRANSCRIBER_PORT')}/{self.language}"
+        BASE_URL = f"http://{os.getenv('TRANSCRIBER_HOST')}:{os.getenv('TRANSCRIBER_PORT')}/{self.language}"
         # headers = {
         #     "Accept": "application/json",
         # }
